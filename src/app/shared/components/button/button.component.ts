92224a1ee3c757753301b4a1c4dccf1e8ae82b88
OlympiqueB <balzsr@gmail.com>
2024-09-24 12:42:20 +0200

    button component

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+import { Component, Input } from "@angular/core";
+import { FaIconLibrary } from "@fortawesome/angular-fontawesome";
+import { fas, IconName } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  selector: "app-button",
+  templateUrl: "./button.component.html",
+  styleUrls: ["./button.component.scss"],
 })
 export class ButtonComponent {
+  private library: FaIconLibrary;
+
   constructor(library: FaIconLibrary) {
+    this.library = library;
     library.addIconPacks(fas);
+  }
+
+  @Input() buttonText?: string;
+  @Input() iconName?: string;
+
+  get icon() {
+    return this.iconName
+      ? this.library.getIconDefinition("fas", this.iconName as IconName)
+      : "";
   }
 
   // Use the names for the inputs `buttonText` and `iconName`.
