4b85e225a2919a5e2961516dd42ae27cee62fa46
OlympiqueB <balzsr@gmail.com>
2024-09-26 12:06:19 +0200

    courses-card added

--- src/app/shared/components/course-card/course-card.component.ts
+++ src/app/shared/components/course-card/course-card.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from "@angular/core";
+import { mockedAuthorsList } from "@app/shared/mocks/mocks";
+
+interface CourseModel {
+  id: string;
+  title: string;
+  description: string;
+  creationDate: string;
+  duration: number;
+  authors: string[];
+}
+
+interface AuthorModel {
+  id: string;
+  name: string;
+}
 
 @Component({
-  selector: 'app-course-card',
-  templateUrl: './course-card.component.html',
-  styleUrls: ['./course-card.component.scss']
+  selector: "app-course-card",
+  templateUrl: "./course-card.component.html",
+  styleUrls: ["./course-card.component.scss"],
 })
-export class CourseCardComponent {}
+export class CourseCardComponent {
+  @Input() courseObject!: CourseModel;
+  @Input() isEditable?: boolean = true;
+
+  getAuthors(authors: string[]): string {
+    const authorsNames: string[] = [];
+
+    authors.forEach((authorId) => {
+      const author = mockedAuthorsList.find((author) => author.id === authorId);
+      if (author) authorsNames.push(author.name);
+    });
+
+    return authorsNames.join(", ");
+  }
+  // @Output() clickOnShow;
+}
