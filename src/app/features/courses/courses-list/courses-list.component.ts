7d418ca5fd706b706c6bc519580263ddcda12bf9
OlympiqueB <balzsr@gmail.com>
2024-09-26 12:07:56 +0200

    courses-list done

--- src/app/features/courses/courses-list/courses-list.component.ts
+++ src/app/features/courses/courses-list/courses-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
+
+interface CourseModel {
+  id: string;
+  title: string;
+  description: string;
+  creationDate: string;
+  duration: number;
+  authors: string[];
+}
 
 @Component({
-  selector: 'app-courses-list',
-  templateUrl: './courses-list.component.html',
-  styleUrls: ['./courses-list.component.css']
+  selector: "app-courses-list",
+  templateUrl: "./courses-list.component.html",
+  styleUrls: ["./courses-list.component.css"],
 })
 export class CoursesListComponent {
-
+  @Input() courses?: CourseModel[];
+  @Input() editable?: boolean = false;
 }
