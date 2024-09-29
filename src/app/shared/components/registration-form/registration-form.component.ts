da0254df8ac07379c56b55d7789789bd88eed93e
OlympiqueB <balzsr@gmail.com>
2024-09-29 14:54:25 +0200

    registration-form done

--- src/app/shared/components/registration-form/registration-form.component.ts
+++ src/app/shared/components/registration-form/registration-form.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-registration-form',
-  templateUrl: './registration-form.component.html',
-  styleUrls: ['./registration-form.component.scss'],
+  selector: "app-registration-form",
+  templateUrl: "./registration-form.component.html",
+  styleUrls: ["./registration-form.component.scss"],
 })
-export class RegistrationFormComponent {
+export class RegistrationFormComponent implements OnInit {
   registrationForm!: FormGroup;
-  // Use the names `name`, `email`, `password` for the form controls.
+  submitted = false;
+
+  ngOnInit(): void {
+    this.registrationForm = new FormGroup({
+      name: new FormControl("", [Validators.required, Validators.minLength(6)]),
+      email: new FormControl("", [Validators.required]),
+      password: new FormControl("", [Validators.required]),
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registrationForm.valid) {
+      console.log(this.registrationForm);
+      console.log("Name:", this.name);
+      console.log("Email:", this.email);
+      console.log("Password:", this.password);
+    } else {
+      console.log(this.registrationForm);
+
+      console.log("Form is invalid");
+    }
+  }
+
+  onRegistrateClick() {
+    const form = document.querySelector("form");
+    const submitEvent = new Event("submit", {
+      bubbles: true,
+      cancelable: true,
+    });
+
+    form?.dispatchEvent(submitEvent);
+  }
+
+  get name() {
+    return this.registrationForm.get("name");
+  }
+  get email() {
+    return this.registrationForm.get("email");
+  }
+  get password() {
+    return this.registrationForm.get("password");
+  }
 }
