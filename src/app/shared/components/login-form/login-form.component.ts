489b364e3b27e2e4f90ad870f4d4198cbdd96026
OlympiqueB <balzsr@gmail.com>
2024-09-29 14:15:21 +0200

    login-form done

--- src/app/shared/components/login-form/login-form.component.ts
+++ src/app/shared/components/login-form/login-form.component.ts
@@ -1,12 +1,30 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { FormControl, NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss'],
+  selector: "app-login-form",
+  templateUrl: "./login-form.component.html",
+  styleUrls: ["./login-form.component.scss"],
 })
 export class LoginFormComponent {
   @ViewChild("loginForm") public loginForm!: NgForm;
-  //Use the names `email` and `password` for form controls.
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log("Email:", this.loginForm.value.email);
+      console.log("Password:", this.loginForm.value.password);
+    } else {
+      console.log("Form is invalid");
+    }
+  }
+
+  onLoginClick() {
+    const form = document.querySelector("form");
+    const submitEvent = new Event("submit", {
+      bubbles: true,
+      cancelable: true,
+    });
+
+    form?.dispatchEvent(submitEvent);
+  }
 }
