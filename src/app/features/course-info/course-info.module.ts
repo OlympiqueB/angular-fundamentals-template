49288837a636951d31f380fca8ec58f326c5b839
OlympiqueB <balzsr@gmail.com>
2024-09-30 13:45:13 +0200

    modules created, updated

---
+++ src/app/features/course-info/course-info.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseInfoComponent } from './course-info.component';
+import { SharedModule } from '@app/shared/shared.module';
+
+const components = [CourseInfoComponent]
+
+@NgModule({
+  declarations: [components],
+  imports: [
+    CommonModule,
+    SharedModule
+  ],
+  exports: [components]
+})
+export class CourseInfoModule { }
