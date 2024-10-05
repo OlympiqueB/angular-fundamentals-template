084f0832d190a370a4040ba12e3a78bea1fbf4fb
OlympiqueB <balzsr@gmail.com>
2024-10-05 13:57:56 +0200

    moved course-list to courses component

--- src/app/features/courses/courses.component.ts
+++ src/app/features/courses/courses.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { mockedCoursesList } from "@app/shared/mocks/mocks";
 
 @Component({
-  selector: 'app-courses',
-  templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  selector: "app-courses",
+  templateUrl: "./courses.component.html",
+  styleUrls: ["./courses.component.scss"],
 })
 export class CoursesComponent {
-
+  mockedCoursesList = mockedCoursesList;
 }
