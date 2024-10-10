7f9791ac262f34c7be7ca7987c86eb8e345b8008
OlympiqueB <balzsr@gmail.com>
2024-10-10 09:10:21 +0200

    added some paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,41 @@
-import { Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 export const routes: Routes = [
-    /* Add your code here */
+  {
+    path: "login",
+    loadChildren: () =>
+      import("./features/login/login.module").then((m) => m.LoginModule),
+  },
+  {
+    path: "registration",
+    loadChildren: () =>
+      import("./features/registration/registration.module").then((m) => m.RegistrationModule),
+  },
+  {
+    path: "courses",
+    loadChildren: () =>
+      import("./features/courses/courses.module").then((m) => m.CoursesModule),
+  },
+  // {
+  //   path: "courses/add",
+  //   loadChildren: () =>
+  //     import("./shared/shared.module").then((m) => m.SharedModule),
+  // },
+  // {
+  //   path: "courses/:id",
+  //   loadChildren: () =>
+  //     import("./shared/shared.module").then((m) => m.SharedModule),
+  // },
+  // {
+  //   path: "courses/edit/:id",
+  //   loadChildren: () =>
+  //     import("./shared/shared.module").then((m) => m.SharedModule),
+  // },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
