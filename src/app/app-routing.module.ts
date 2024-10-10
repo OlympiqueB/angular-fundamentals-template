ee1b54bf363388cd2023a22bd2a65e8abfe026fd
OlympiqueB <balzsr@gmail.com>
2024-10-10 09:26:33 +0200

    courses routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 export const routes: Routes = [
-  { path: "", redirectTo: "courses", pathMatch: "prefix" },
   {
     path: "login",
     loadChildren: () =>
