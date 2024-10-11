18907bf3978385f688e61178ae03ab66bb5db4a2
OlympiqueB <balzsr@gmail.com>
2024-10-11 18:40:19 +0200

    removed mock, made it async

--- src/app/shared/pipes/author-list.pipe.ts
+++ src/app/shared/pipes/author-list.pipe.ts
@@ -1,18 +1,25 @@
 import { Pipe } from "@angular/core";
-import { mockedAuthorsList } from "../mocks/mocks";
+import { CoursesStoreService } from "@app/services/courses-store.service";
+import { map } from "rxjs";
 
 @Pipe({
   name: "authorList",
+  pure: false,
 })
 export class AuthorListPipe {
+  constructor(private coursesStoreService: CoursesStoreService) {}
+
   transform(authorList: string[]) {
-    const authorsNames: string[] = [];
+    return this.coursesStoreService.authors$.pipe(
+      map((authors) => {
+        const authorsNames: string[] = [];
+        authorList.forEach((authorId) => {
+          const author = authors.find((author) => author.id === authorId);
+          if (author) authorsNames.push(author.name);
+        });
 
-    authorList.forEach((authorId) => {
-      const author = mockedAuthorsList.find((author) => author.id === authorId);
-      if (author) authorsNames.push(author.name);
-    });
-
-    return authorsNames.join(", ");
+        return authorsNames.join(", ");
+      })
+    );
   }
 }
