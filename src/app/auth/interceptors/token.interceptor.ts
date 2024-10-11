ea1844ade3bc0a48e222b95cd8e72f520ee06780
OlympiqueB <balzsr@gmail.com>
2024-10-11 13:18:22 +0200

    interceptor added

--- src/app/auth/interceptors/token.interceptor.ts
+++ src/app/auth/interceptors/token.interceptor.ts
@@ -18,23 +18,24 @@ export class TokenInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     const token = this.sessionStorageService.getToken();
-
     let authReq = req;
+    
     if (token) {
       authReq = req.clone({
         setHeaders: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `${token}`,
         },
       });
     }
 
     return next.handle(authReq).pipe(
       catchError((error: HttpErrorResponse) => {
-        if (error.status === 401) {
+        if (error.status === 401 && !req.headers.has('skipAuthInterceptor')) {
           this.authService.logout().subscribe(() => {
             this.router.navigate(["/login"]);
           });
         }
+        console.log(authReq)
         return throwError(error);
       })
     );
