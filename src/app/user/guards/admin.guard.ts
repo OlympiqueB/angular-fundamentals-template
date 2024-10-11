bbaa3ba5c9ab3c3e25dcd024add5834cffc05b3e
OlympiqueB <balzsr@gmail.com>
2024-10-11 15:17:26 +0200

    admin guard

--- src/app/user/guards/admin.guard.ts
+++ src/app/user/guards/admin.guard.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserStoreService } from '../services/user-store.service';
 
 @Injectable({
     providedIn: 'root'
 })
-export class AdminGuard {
-    // Add your code here
+export class AdminGuard implements CanActivate {
+  constructor(private userStoreService: UserStoreService, private router: Router) {}
+
+  canActivate():
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const isAdmin = this.userStoreService.isAdmin;
+
+    if (!isAdmin) {
+      return true;
+    } else {
+      return this.router.createUrlTree(["/courses"]);
+    }
+  }
 }
