927dc0766093f10391d2e9547c515a87ad7fd459
OlympiqueB <balzsr@gmail.com>
2024-10-11 13:20:27 +0200

    interceptor added to providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { SharedModule } from '@shared/shared.module';
-import { AppComponent } from '@app/app.component';
-import { NotAuthorizedGuard } from '@app/auth/guards/not-authorized.guard';
-import { AuthorizedGuard } from '@app/auth/guards/authorized.guard';
-import { CoursesStoreService } from '@app/services/courses-store.service';
-import { CoursesService } from '@app/services/courses.service';
-import { CoursesModule } from './features/courses/courses.module';
-import { CourseInfoModule } from './features/course-info/course-info.module';
-import { ButtonLabelService } from './services/button-label.service';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { SharedModule } from "@shared/shared.module";
+import { AppComponent } from "@app/app.component";
+import { NotAuthorizedGuard } from "@app/auth/guards/not-authorized.guard";
+import { AuthorizedGuard } from "@app/auth/guards/authorized.guard";
+import { CoursesStoreService } from "@app/services/courses-store.service";
+import { CoursesService } from "@app/services/courses.service";
+import { CoursesModule } from "./features/courses/courses.module";
+import { CourseInfoModule } from "./features/course-info/course-info.module";
+import { ButtonLabelService } from "./services/button-label.service";
+import { AppRoutingModule } from "./app-routing.module";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { TokenInterceptor } from "./auth/interceptors/token.interceptor";
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,9 +23,20 @@ import { HttpClientModule } from '@angular/common/http';
     CoursesModule,
     CourseInfoModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
   ],
-  providers: [AuthorizedGuard, NotAuthorizedGuard, CoursesService, CoursesStoreService, ButtonLabelService],
+  providers: [
+    AuthorizedGuard,
+    NotAuthorizedGuard,
+    CoursesService,
+    CoursesStoreService,
+    ButtonLabelService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
